3d65468d3a01e271158abe7d268bd68c98c8b033
kamil <kamil271e@gmail.com>
2024-04-27 15:22:26 +0200

    Estimating probability thresholds with bayesian optimization

--- comparator/hybiak.py
+++ comparator/hybiak.py
@@ -6,10 +6,16 @@ from player import Player
 
 class Hybiak(Player):
 
-    def __init__(self, name):
+    def __init__(self, name, p85=0.85, p80=0.80, p75=0.75, p70=0.70, p60=0.60, p50=0.50):
         super().__init__(name)
         self.played_cards = []
         self.moves = 0
+        self.p85 = p85
+        self.p80 = p80
+        self.p75 = p75
+        self.p70 = p70
+        self.p60 = p60
+        self.p50 = p50
 
     def _find_free_color(self, value):
         colors = [0, 1, 2, 3]
@@ -58,16 +64,14 @@ class Hybiak(Player):
                             if color is not None:
                                 my_declaration = (bigger_needed_value, color)
                             else:
-                                my_declaration = (needed_value, my_declaration[1])
+                                my_declaration = (needed_value, my_declaration[1]) # "draw" ?
 
         self.played_cards.append(min_card)
         return min_card, my_declaration
 
-
-
-
     def checkCard(self, opponent_declaration):
-        
+        # TODO: dodac jakies warunki sprawdzajace ile kart ma przeciwnik
+
         if opponent_declaration is None:
             return False
 
@@ -75,35 +79,34 @@ class Hybiak(Player):
             return True
         
         if opponent_declaration[0] == 14:
-            random = np.random.choice([True, False], p=[0.50, 0.50])
+            random = np.random.choice([True, False], p=[self.p50, 1-self.p50])
             if random:
                 return True
             elif len(self.cards) == 1:
-                return np.random.choice([True, False], p=[0.85, 0.15])
-            
-        
+                return np.random.choice([True, False], p=[self.p85, 1-self.p85])
+
         if opponent_declaration[0] > self.cards[0][0] + 2:
             return True
         
         if opponent_declaration[0] > self.cards[0][0] + 1:
-            return np.random.choice([True, False], p=[0.85, 0.15])
+            return np.random.choice([True, False], p=[self.p85, 1-self.p85])
         
         if opponent_declaration[0] > self.cards[0][0]:
             if len(self.cards) > 1:
                 if len(self.cards) == 2:
                     return True
                 if len(self.cards) == 3:
-                    return np.random.choice([True, False], p=[0.85, 0.15])
+                    return np.random.choice([True, False], p=[self.p85, 1-self.p85])
                 if len(self.cards) == 4:
-                    return np.random.choice([True, False], p=[0.80, 0.20])
+                    return np.random.choice([True, False], p=[self.p80, 1-self.p80])
             else:
-                return np.random.choice([True, False], p=[0.70, 0.30])
+                return np.random.choice([True, False], p=[self.p70, 1-self.p70])
 
         if opponent_declaration in self.played_cards:
             if self.moves < 2:
                 return True
             if self.moves < 3:
-                return np.random.choice([True, False], p=[0.60, 0.40])
+                return np.random.choice([True, False], p=[self.p60, 1-self.p60])
             else:
-                return np.random.choice([True, False], p=[0.250, 0.750])
+                return np.random.choice([True, False], p=[1-self.p75, self.p75])
         return False
